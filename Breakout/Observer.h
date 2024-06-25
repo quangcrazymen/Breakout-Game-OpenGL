5c927a51bf79c5cdb97894a97204371c26a53899
Quang <quang.nguyendo@gameloft.com>
2024-06-25 15:19:49 +0700

    - Clean up project - Update Readme.md

--- Breakout/Observer.h
+++ Breakout/Observer.h
@@ -7,6 +7,5 @@ class Observer
 public:
 	virtual ~Observer() {}
 	virtual void onNotify(const GameObject& entity, Event event) = 0;
-	//virtual void onNotify(const GameObject& entity, Event event);
 };
 
